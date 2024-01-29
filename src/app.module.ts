08943a6c0168a01058005befd7ddcf4973a056a7
Habib Syuhada <habibsumedang@gmail.com>
2024-01-29 12:58:37 +0700

    User Data Basic Crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+		UsersModule,
+		ConfigModule.forRoot(),
+		TypeOrmModule.forRoot({
+			type: process.env.DBTYPE as any,
+			host: process.env.DBHOST,
+			port: parseInt(process.env.DBPORT),
+			username: process.env.DBUSER,
+			password: process.env.DBPASSWORD,
+			database: process.env.DBNAME,
+			entityPrefix: process.env.PREFIXTABLE,
+			ssl: true,
+			entities: [__dirname + '/**/*.entity{.ts,.js}'],
+			synchronize: true,
+		}),
+	],
   controllers: [AppController],
   providers: [AppService],
 })
